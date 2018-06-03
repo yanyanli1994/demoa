721da1da111806e7e82f4537d32ba47d03ea2222
yanyanli1994 <1270203234@qq.com>
2018-06-03 16:26:47 +0800

    Terry老师教程

---
+++ src/test/java/TestModule1/TestMavenSureFirePlugin.java
@@ -0,0 +1,44 @@
+package TestModule1;
+
+import junit.framework.Assert;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import utils.Browsers;
+import utils.BrowsersType;
+
+
+
+public class TestMavenSureFirePlugin {
+
+	private WebDriver driver;
+
+	
+	@BeforeClass
+	public void beforeClass(){
+		Browsers browser = new Browsers(BrowsersType.chrome);
+		driver = browser.driver;
+	}
+
+	@Test
+	public void testLinkCase(){
+		
+		
+		driver.get("http://www.126.com");
+		driver.findElement(By.xpath("//input[@id='idInput']")).clear();
+		driver.findElement(By.xpath("//input[@id='idInput']")).sendKeys("FireflyAutomation");
+		
+		driver.findElement(By.xpath("//input[@id='pwdInput']")).sendKeys("Firefly");
+		driver.findElement(By.xpath("//input[@id='pwdInput']")).submit();		
+		Assert.assertEquals(true, true);
+	}
+	
+	@AfterClass
+	public void quit(){
+		driver.quit();
+	}
+}
